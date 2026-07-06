6843f4104bd533a0477897f1996f28a226817695
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-07 04:59:35 +0700

    Refactor MinmutVoice to load and set voice

--- js/voice.js
+++ js/voice.js
@@ -1,5 +1,33 @@
 class MinmutVoice {
 
+    constructor(){
+        this.voice = null;
+
+        speechSynthesis.onvoiceschanged = () => {
+            this.loadVoice();
+        };
+
+        this.loadVoice();
+    }
+
+    loadVoice(){
+
+        const voices = speechSynthesis.getVoices();
+
+        this.voice =
+            voices.find(v =>
+                v.lang.startsWith("id") &&
+                v.name.toLowerCase().includes("male")
+            ) ||
+
+            voices.find(v =>
+                v.lang.startsWith("id")
+            ) ||
+
+            null;
+
+    }
+
     speak(text){
 
         speechSynthesis.cancel();
@@ -7,6 +35,10 @@ class MinmutVoice {
         const u = new SpeechSynthesisUtterance(text);
 
         u.lang = "id-ID";
+
+        if(this.voice){
+            u.voice = this.voice;
+        }
 
         speechSynthesis.speak(u);
 
