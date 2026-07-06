5d98de50c834b78737dea7590cd161e43066480c
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-07 05:29:28 +0700

    Add MinmutAvatar class with action methods
    Implemented MinmutAvatar class with methods for actions.

---
+++ js/avatar.js
@@ -0,0 +1,41 @@
+class MinmutAvatar {
+
+    constructor() {
+
+        this.minmut = null;
+
+    }
+
+    init() {
+
+        this.minmut = document.getElementById("minmut");
+
+    }
+
+    blink() {
+
+        console.log("Blink");
+
+    }
+
+    happy() {
+
+        console.log("Happy");
+
+    }
+
+    thinking() {
+
+        console.log("Thinking");
+
+    }
+
+    wave() {
+
+        console.log("Wave");
+
+    }
+
+}
+
+window.MinmutAvatar = MinmutAvatar;
