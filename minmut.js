8604570cd6315846fd99e532ad0ba79f2550e2bb
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-07 01:27:14 +0700

    Test GitHub Engine

--- minmut.js
+++ minmut.js
@@ -1,10 +1,23 @@
 console.log("MINMUT Engine 2.0 Loaded");
 
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-const m=document.createElement("div");
-m.id="minmut";
-m.innerHTML='<img src="assets/minmut.png" alt="Minmut">';
-document.body.appendChild(m);
+    const test = document.createElement("div");
+
+    test.innerHTML = "🐹 MINMUT ENGINE 2.0";
+
+    test.style.position = "fixed";
+    test.style.bottom = "20px";
+    test.style.right = "20px";
+    test.style.background = "#16a34a";
+    test.style.color = "#ffffff";
+    test.style.padding = "12px 18px";
+    test.style.borderRadius = "14px";
+    test.style.boxShadow = "0 8px 20px rgba(0,0,0,.25)";
+    test.style.fontFamily = "Arial,sans-serif";
+    test.style.fontWeight = "bold";
+    test.style.zIndex = "999999";
+
+    document.body.appendChild(test);
 
 });
