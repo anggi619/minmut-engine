ecb7f7d77907ffac8e7e9808c82824d508c8b42f
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-09 00:34:45 +0700

    Refactor MinmutAvatarAnimation class methods

--- js/avatar-animation.js
+++ js/avatar-animation.js
@@ -2,34 +2,69 @@ class MinmutAvatarAnimation {
 
     constructor(avatar){
 
-    this.avatar = avatar;
+        this.avatar = avatar;
 
-    this.base = "https://anggi619.github.io/minmut-engine/assets/";
+        this.base = "https://anggi619.github.io/minmut-engine/assets/";
 
-    this.registry = new MinmutAnimationRegistry();
+        this.registry = new MinmutAnimationRegistry();
 
-    this.queue = [];
+        this.queue = [];
 
-    this.running = false;
+        this.running = false;
 
-}
+    }
 
     sleep(ms){
 
-        enqueue(name){
-
-    this.queue.push(name);
-
-    this.runQueue();
-
-}
         return new Promise(resolve => setTimeout(resolve, ms));
+
+    }
+
+    enqueue(name){
+
+        this.queue.push(name);
+
+        this.runQueue();
+
+    }
+
+    play(name){
+
+        this.enqueue(name);
+
+    }
+
+    async runQueue(){
+
+        if(this.running) return;
+
+        this.running = true;
+
+        while(this.queue.length > 0){
+
+            const name = this.queue.shift();
+
+            const animation = this.registry.get(name);
+
+            if(animation){
+
+                await this.playFrames(animation);
+
+            }else{
+
+                console.warn("Animation tidak ditemukan :", name);
+
+            }
+
+        }
+
+        this.running = false;
 
     }
 
     async playFrames(animation){
 
-        for(let i=1;i<=animation.frames;i++){
+        for(let i = 1; i <= animation.frames; i++){
 
             this.avatar.setImage(
 
@@ -51,22 +86,6 @@ class MinmutAvatarAnimation {
             this.base + "idle/idle1.png"
 
         );
-
-    }
-
-    async play(name){
-
-        const animation = this.registry.get(name);
-
-        if(!animation){
-
-            console.warn("Animation tidak ditemukan:", name);
-
-            return;
-
-        }
-
-        await this.playFrames(animation);
 
     }
 
