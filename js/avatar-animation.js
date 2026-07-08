81332d73734c1a402bb358a455cb8e2fdc69063f
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-08 22:20:17 +0700

    Update avatar-animation.js

--- js/avatar-animation.js
+++ js/avatar-animation.js
@@ -4,7 +4,13 @@ class MinmutAvatarAnimation {
 
         this.avatar = avatar;
 
-        this.base = "https://anggi619.github.io/minmut-engine/";
+        this.base = "https://anggi619.github.io/minmut-engine/assets/";
+
+    }
+
+    sleep(ms){
+
+        return new Promise(resolve => setTimeout(resolve, ms));
 
     }
 
@@ -26,62 +32,28 @@ class MinmutAvatarAnimation {
 
     }
 
-    sleep(ms){
-
-        return new Promise(r=>setTimeout(r,ms));
-
-    }
-
     async wave(){
 
-        this.avatar.setImage(this.base+"wave/wave1.png");
+        this.avatar.setImage(this.base + "wave/wave1.png");
 
         await this.sleep(180);
 
-        this.avatar.setImage(this.base+"wave/wave2.png");
+        this.avatar.setImage(this.base + "wave/wave2.png");
 
         await this.sleep(180);
 
-        this.avatar.setImage(this.base+"wave/wave1.png");
+        this.avatar.setImage(this.base + "wave/wave1.png");
 
         await this.sleep(180);
 
-        this.avatar.setImage(this.base+"wave/wave2.png");
+        this.avatar.setImage(this.base + "wave/wave2.png");
 
         await this.sleep(180);
 
-        this.avatar.setImage(this.base+"assets/minmut.png");
+        this.avatar.setImage(this.base + "idle/idle.png");
 
     }
 
 }
-async play(name){
 
-    switch(name){
-
-        case "wave":
-
-            this.avatar.setImage("wave/wave1.png");
-
-            await this.sleep(150);
-
-            this.avatar.setImage("wave/wave2.png");
-
-            await this.sleep(150);
-
-            this.avatar.setImage("wave/wave1.png");
-
-            await this.sleep(150);
-
-            this.avatar.setImage("idle/idle.png");
-
-            break;
-
-    }
-sleep(ms){
-
-    return new Promise(resolve=>setTimeout(resolve,ms));
-
-}
-}
 window.MinmutAvatarAnimation = MinmutAvatarAnimation;
