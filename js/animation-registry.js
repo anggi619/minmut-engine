e193d9bd95759e981faa143f6f74a1656ac7fced
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-09 01:04:48 +0700

    Update animation-registry.js

--- js/animation-registry.js
+++ js/animation-registry.js
@@ -4,50 +4,56 @@ class MinmutAnimationRegistry {
 
         this.animations = {
 
-            idle:{
-                folder:"idle",
-                speed:1000,
-                sequence:[1]
-            },
+    idle:{
+        folder:"idle",
+        speed:1000,
+        sequence:[1],
+        returnIdle:false
+    },
 
-            blink:{
-                folder:"blink",
-                speed:180,
-                sequence:[1,2,1]
-            },
+    blink:{
+        folder:"blink",
+        speed:180,
+        sequence:[1,2,1],
+        returnIdle:true
+    },
 
-            wave:{
-                folder:"wave",
-                speed:450,
-                sequence:[1,2,1,2,1,2]
-            },
+    wave:{
+        folder:"wave",
+        speed:450,
+        sequence:[1,2,1,2,1,2],
+        returnIdle:true
+    },
 
-            talk:{
-                folder:"talk",
-                speed:150,
-                sequence:[1,2,3,2,1,2,3,2]
-            },
+    talk:{
+        folder:"talk",
+        speed:150,
+        sequence:[1,2,3,2,1,2,3,2],
+        returnIdle:false
+    },
 
-            happy:{
-                folder:"happy",
-                speed:350,
-                sequence:[1,2,3,2,1]
-            },
+    happy:{
+        folder:"happy",
+        speed:350,
+        sequence:[1,2,3,2,1],
+        returnIdle:true
+    },
 
-            thinking:{
-                folder:"thinking",
-                speed:500,
-                sequence:[1,2,3,2,1]
-            },
+    thinking:{
+        folder:"thinking",
+        speed:500,
+        sequence:[1,2,3,2,1],
+        returnIdle:true
+    },
 
-            sad:{
-                folder:"sad",
-                speed:450,
-                sequence:[1,2,3,2,1]
-            }
+    sad:{
+        folder:"sad",
+        speed:450,
+        sequence:[1,2,3,2,1],
+        returnIdle:true
+    }
 
-        };
-
+};
     }
 
     get(name){
