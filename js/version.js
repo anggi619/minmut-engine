b8f10133f2bee2be00570e0f808d2e9b97c09860
Anggi Pratama <61521299+anggi619@users.noreply.github.com>
2026-07-10 00:06:13 +0700

    Add MINMUT version information and logging

---
+++ js/version.js
@@ -0,0 +1,27 @@
+window.MINMUT_VERSION = {
+
+    major : 3,
+
+    minor : 2,
+
+    patch : 0,
+
+    status : "Stable",
+
+    build : "2026.07"
+
+};
+
+console.log(
+
+    "MINMUT Engine",
+
+    MINMUT_VERSION.major + "." +
+
+    MINMUT_VERSION.minor + "." +
+
+    MINMUT_VERSION.patch,
+
+    MINMUT_VERSION.status
+
+);
